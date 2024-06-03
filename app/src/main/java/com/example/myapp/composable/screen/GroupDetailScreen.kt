c0f259d3fe8753457c1018aacd045b290b28f245
kmkm0318 <kmkm0318@naver.com>
2024-06-03 19:35:01 +0900

    GroupDetailScreen Update and Tested On MyViewModel

--- app/src/main/java/com/example/myapp/composable/screen/GroupDetailScreen.kt
+++ app/src/main/java/com/example/myapp/composable/screen/GroupDetailScreen.kt
@@ -1,5 +1,6 @@
 package com.example.myapp.composable.screen
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,50 +19,54 @@ import com.example.myapp.composable.component.ShowThumbnail
 import com.example.myapp.data.MyViewModel
 
 @Composable
-fun GroupDetailScreen(myViewModel: MyViewModel, itemIndex:Int?) {
-    Column {
-        
+fun GroupDetailScreen(myViewModel: MyViewModel, itemIndex: Int?) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(12.dp)
+    ) {
         //정보 제공 화면
-        Image(
-            painter = painterResource(id = myViewModel.dataList[itemIndex!!].showImg),
-            contentDescription = myViewModel.dataList[itemIndex!!].bandName,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .fillMaxWidth()
-        )
-        Text(text = myViewModel.dataList[itemIndex!!].bandName, fontSize = 50.sp, modifier = Modifier.padding(bottom = 25.dp))
-
+        item {
+            Image(
+                painter = painterResource(id = myViewModel.dataList[itemIndex!!].showImg),
+                contentDescription = myViewModel.dataList[itemIndex!!].bandName,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+        }
         //지도는 여기에 추가해주세요
-        
+        item {
+            Text(
+                text = myViewModel.dataList[itemIndex!!].bandName,
+                fontSize = 50.sp,
+                modifier = Modifier.padding(bottom = 25.dp)
+            )
+        }
+        item {
+            Text(text = "${myViewModel.dataList[itemIndex!!].bandName}의 음악 리스트", fontSize = 25.sp)
+        }
         //음악, 쇼츠 리스트화면
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(12.dp)
-        ) {
-            item {
-                Text(text = "${myViewModel.dataList[itemIndex!!].bandName}의 음악 리스트", fontSize = 25.sp)
-            }
-            item {
-                ShowThumbnail(
-                    videoIds = myViewModel.dataList[itemIndex!!].musicList,
-                    Modifier
-                        .size(320.dp, 180.dp)
-                        .padding(8.dp)
-                )
-            }
-            item {
-                Text(text = "${myViewModel.dataList[itemIndex!!].bandName}의 쇼츠 리스트", fontSize = 25.sp)
-            }
-            item {
-                ShowThumbnail(
-                    videoIds = myViewModel.dataList[itemIndex!!].shortList,
-                    Modifier
-                        .size(160.dp, 160.dp)
-                        .padding(8.dp)
-                )
-            }
+        item {
+            ShowThumbnail(
+                videoIds = myViewModel.dataList[itemIndex!!].musicList,
+                Modifier
+                    .size(320.dp, 180.dp)
+                    .padding(8.dp)
+            )
+        }
+        item {
+            Text(text = "${myViewModel.dataList[itemIndex!!].bandName}의 쇼츠 리스트", fontSize = 25.sp)
+        }
+        item {
+            ShowThumbnail(
+                videoIds = myViewModel.dataList[itemIndex!!].shortList,
+                Modifier
+                    .size(160.dp, 160.dp)
+                    .padding(8.dp)
+            )
         }
     }
+
 
 }
--- app/src/main/java/com/example/myapp/data/MyViewModel.kt
+++ app/src/main/java/com/example/myapp/data/MyViewModel.kt
@@ -1,16 +1,26 @@
 package com.example.myapp.data
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 
-class MyViewModel(private val application:Application) : AndroidViewModel(application) {
+class MyViewModel(private val application: Application) : AndroidViewModel(application) {
     var dataList = mutableListOf<AllData>()
         private set
 
     init {
+        //밴드 디테일 화면 실험용
+        val bandName = "제시 바베라"
+        val musicList =
+            listOf("nh29uTWeK3A", "p5MSR2-SLd4", "fACfypWLqXg", "JY3BEc2j1NI", "qExLcd2ZYjA")
+        val shortList =
+            listOf("5DDBkBcN43E", "4-5A2S09FGg", "kvaCtrdGn-w", "DvIX9IAVF1w", "g6wZAa7FAI8")
+
+        val tmp = AllData(bandName = bandName, musicList = musicList, shortList = shortList)
+        dataList.add(tmp)
+
         //임시 데이터
         dataList.add(AllData("INDI STREET"))
-        for(i: Int in 1..30)
-            dataList.add(AllData())
+        for (i: Int in 1..30) dataList.add(AllData())
     }
 }
