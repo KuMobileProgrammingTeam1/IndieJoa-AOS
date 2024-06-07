2da49617b15785ac02d5e4e662b1af303e14dc2f
kmkm0318 <kmkm0318@naver.com>
2024-06-07 15:44:45 +0900

    Input Test Update

--- app/src/main/java/com/example/myapp/Retrofit/TestRetrofit.kt
+++ app/src/main/java/com/example/myapp/Retrofit/TestRetrofit.kt
@@ -3,24 +3,30 @@ package com.example.myapp.Retrofit
 import android.util.Log
 import retrofit2.*
 
-fun TestRetrofit(){
+fun TestRetrofit() {
     val retrofitService = RetrofitFactory.create()
 
-    val call = retrofitService.getAllArtists(page = 0, size = 10)
+    val call = retrofitService.getAllArtists(page = 0, size = 5000)
 
-    call.enqueue(object : Callback<ArtistResponse>{
+    call.enqueue(object : Callback<ArtistResponse> {
         override fun onResponse(call: Call<ArtistResponse>, response: Response<ArtistResponse>) {
-            if(response.isSuccessful){
+            if (response.isSuccessful) {
                 val artistResponse = response.body()
                 println("testRetrofit OnResponse")
                 println("${artistResponse?.totalElements}")
 
-                artistResponse?.content?.forEach{
-                    artist->
-                    println("artistName : ${artist.name}")
+                artistResponse?.content?.forEach { artist ->
+                    if (artist.name == "이런") {
+                        println("artistName : ${artist.name}")
+                        println("artistID : ${artist.artistId}")
+                        println("youtubeVideoLink : ${artist.youtubeVideoLink}")
+                        TestRetrofitInput(artist)
+                        println("artistName : ${artist.name}")
+                        println("artistID : ${artist.artistId}")
+                        println("youtubeVideoLink : ${artist.youtubeVideoLink}")
+                    }
                 }
-            }
-            else{
+            } else {
                 println("failed on response")
             }
         }
@@ -31,6 +37,25 @@ fun TestRetrofit(){
     })
 }
 
-fun main(){
+fun TestRetrofitInput(artist: ArtistData) {
+    val retrofitService = RetrofitFactory.create()
+
+    val tmp = artist.copy(youtubeVideoLink = "test")
+
+    val call = retrofitService.updateArtist(tmp)
+
+    call.enqueue(object : Callback<Unit> {
+        override fun onResponse(p0: Call<Unit>, p1: Response<Unit>) {
+            println("Success to Update ${artist.name}")
+        }
+
+        override fun onFailure(p0: Call<Unit>, p1: Throwable) {
+            println("Fail to Update ${artist.name}")
+        }
+
+    })
+}
+
+fun main() {
     TestRetrofit()
 }
